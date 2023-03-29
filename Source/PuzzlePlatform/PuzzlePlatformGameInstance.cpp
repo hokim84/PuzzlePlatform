d4a8dedf1b4cb1f3ea7ec07bbde265357ca136ea
DESKTOP-K38EF38\hodol <hodk84@naver.com>
2023-03-30 04:40:58 +0900

    게임인스턴스

---
+++ Source/PuzzlePlatform/PuzzlePlatformGameInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformGameInstance.h"
+#include "Engine/Engine.h"
+
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+}
+
+void UPuzzlePlatformGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hostring"));
+
+}
+
+
+void UPuzzlePlatformGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Blue, FString::Printf(TEXT("Joining %s"), *Address));
+
+}
